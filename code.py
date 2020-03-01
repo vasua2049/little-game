30f4cae6983e11eba65b703845c52397f149ba2e
vasua2049 <61631763+vasua2049@users.noreply.github.com>
2020-03-01 07:33:07 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 # little-game
 #it is tle little game to work out with functions. this is one of the task  in the book of Zed Shaw that i read
+#my english is not good
 
 from sys import exit
 def white_rose():
